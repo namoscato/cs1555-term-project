45577381705f414dacd2a20b6a5f8db47e2a9f8a
Nicholas Amoscato <nicholas@amoscato.com>
2013-03-24 18:03:13 -0400

    Check if category is valid when adding product

--- functions.sql
+++ functions.sql
@@ -2,6 +2,7 @@
 create or replace type vcarray as varray(50) of varchar2(20);
 /
 
+-- procedure inserts a product into the database
 create or replace procedure put_product (
   name in varchar2,
   description in varchar2,
@@ -11,14 +12,31 @@ create or replace procedure put_product (
   min_price in int,
   id out int
 ) is
+  i number;
   new_id int;
   start_date date;
+  invalid_cat exception;
 begin
   select my_time into start_date
   from sys_time;
+
+  -- check to see if categories are valid
+  i := categories.FIRST;
+  loop
+    exit when i is null;
+    if ((select count(name) from category where parent_category = categories(i)) = 0) 
+      then raise invalid_cat;
+    end if;
+    i := categories.NEXT(i);
+  end loop;
+
   insert into product values(1, name, description, seller, start_date, min_price, days, 'underauction', null, null, null) returning auction_id into new_id;
   return;
+exception
+  when invalid_cat then raise_application_error(-20001, 'category is invalid');
 end;
 /
 
--- execute put_product('test', null, vcarray('Laptops', 'Math'), 2, 'user0', 10);
+commit;
+
+-- execute put_product('test', 'testing', vcarray('Laptops', 'Math'), 2, 'user0', 10);
