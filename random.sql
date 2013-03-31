7537e42a8d068cc0cfb6b46abe12f92dc39fe662
Josh <jtf15@pitt.edu>
2013-03-31 14:27:30 -0400

    Fixed random stuff
    Changed a lot of the random queries to work exactly as the email Roxana
    sent wants them to

--- random.sql
+++ random.sql
@@ -3,43 +3,43 @@
 
 --1. Customer Interface
 --(a) Browsing products
-----Java: String X = user input for category
+----Java: Assuming 'books' was the category given
 select auction_id as "Auction ID", name as "Name", description as "Description"    
-from product where status = 'underauction' and auction_id is in
-	(select auction_id from belongs to where category = *X*) ;
+from product where status = 'underauction' and auction_id in
+	(select auction_id from belongsto where category = 'books') ;
 ----Prompt user if they want to sort by highest bid (a), alphabetically by product name(b), or exit:
 --------a:
 select auction_id as "Auction ID", name as "Name", 
 description as "Description", amount as "Highest Bid"  
-from product where status = 'underauction' and auction_id is in
-	(select auction_id from belongs to where category = *X*) 
+from product where status = 'underauction' and auction_id in
+	(select auction_id from belongsto where category = 'books') 
 order by amount desc ;
 --------b:
 select auction_id as "Auction ID", name as "Name", description as "Description"    
 from product where status = 'underauction' and auction_id is in
-	(select auction_id from belongs to where category = *X*) 
+	(select auction_id from belongsto where category = 'books') 
 order by name desc ;
 
 
 --(b) Searching for product by text
-----Java stuff to check if they entered one or two keywords (X, Y)
+----Java: Assuming 'DELL' and 'INSPIRON' are the two keywords given
 ------If one keyword:
 select auction_id as "Auction ID", name as "Name", description as "Description"    
-from product where description like "%X%" ;
+from product where upper(description )like upper('%dell%') ;
 ------If two keywords:
 select auction_id as "Auction ID", name as "Name", description as "Description"    
-from product where description like "%X%Y%" or "%Y%X" ;
+from product where upper(description) like upper('%dell%') and upper(description) like upper('%inspiron%') ;
 
 --(e) Let the seller withdraw or sell the product for the 2nd highest price 
 --I UPDATED THE PRODUCT TABLE AND ONE OF THE TRIGGERS OR THIS -- I ADDED THE AMOUNT2 ATTRIBUTE
 --when an auction ends. The assumes that the seller of the ended product is logged in.
 ----Java to check each auction_id of closed auctions to see if the logged in seller is the seller of that product.
-----Java to ask whether they want to sell or withdraw the product given the amount2 value.
+----Java: Assuming auction_id is 1 
 -------If the seller sells:
-update product set amount = amount2 where amount2 > 0 and auction_id = X;
-update product set status = "sold" where auction_id = X;
+update product set amount = amount2 where amount2 > 0 and auction_id = 1;
+update product set status = "sold" where auction_id = 1;
 -------If the seller withdraws:
-update product set status = "withdrawn" where auction_id = X ;
+update product set status = "withdrawn" where auction_id = 1;
 
 --(f) Suggestions
 -- If a customer X asks, the system can provide suggestions on which products he/she should look
@@ -71,9 +71,16 @@ select auction_id from (
 --2. Administrator Interface
 --(a) New Customer registration
 --	Ask whether or not the new person is a customer or admin and run the following:
-insert into customer/administrator values(X, Y, Z, I, J) ;
+--	Assume the following data was entered: 
+insert into customer values('TestUser', 'Password', 'Joe Smith', '123 Fake Str', 'joe@smith.com') ;
 
 --(b) Update System Date
 --	Ask user to input new system date in dd-mm-yyyy/hh:mi:ssam format:
-update sys_time set my_time = X ;
+update sys_time set my_time = to_date('12-dec-2012/09:00:00pm', 'dd-mm-yyyy/hh:mi:ssam');
 
+--4. Additional Function Requirement
+--	a. SQL statement to return user info given a login name/password
+--		Java: assuming data given was usn: 'TestUser' pwd: 'Password'
+select login, password, name, address, email 
+from customer where login = 'TestUse' and password = 'Password' ;
+--	b. closeAuctions is already made in generate.sql
