6cbbc12a8ea05c1f9993474c3a4c0321f36fdeb6
Nicholas Amoscato <nicholas@amoscato.com>
2013-04-11 17:06:36 -0400

    Simplify user input and outline admin/user menus

--- src/MyAuction.java
+++ src/MyAuction.java
@@ -11,8 +11,69 @@ public class MyAuction {
 	private ResultSet resultSet; //used to hold the result of your query (if one // exists)
 	private String query;  //this will hold the query we are using
 	private String username, password;
+	private Scanner input;
 	
 	private static final String HR = "--------------------------------------";
+	
+	public MyAuction() {
+		try {
+			// get username and password
+			Scanner scanner = new Scanner(new File("db.config"));
+			username = scanner.nextLine();
+			password = scanner.nextLine();
+			scanner.close();
+			
+			try {
+				// register oracle driver and connect to db
+				// must be on unixs.cis.pitt.edu and run "source ~panos/1555/bash.env"
+				DriverManager.registerDriver (new oracle.jdbc.driver.OracleDriver());
+				connection = DriverManager.getConnection("jdbc:oracle:thin:@db10.cs.pitt.edu:1521:dbclass", username, password);
+				input = new Scanner(System.in);
+				
+				System.out.println("Welcome to MyAuction!");
+				promptMenu(0);
+			}
+			catch(Exception e)  {
+				System.err.println("Error connecting to database: " + e.toString());
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("Could not find db.config file: " + e.toString());
+		}
+	}
+	
+	/*
+	 * @param prompt descriptive prompt of input
+	 * @return trimmed line of user input
+	 */
+	public String getUserInput(String prompt) {
+		System.out.print(prompt + ": ");
+		return input.nextLine().trim();
+	}
+	
+	/*
+	 * @param title title of menu
+	 * @param choices list of menu choices
+	 * @param prompt descriptive prompt for user input
+	 */
+	public int getUserChoice(String title, List<String> choices, String prompt) {
+		// print choices
+		System.out.println(prompt + "\n" + HR);
+		for (int i = 1; i <= choices.size(); i++) {
+			System.out.println("  " + i + ") " + choices.get(i - 1));
+		}
+		System.out.println(HR);
+		
+		// prompt user for input
+		int choice;
+		do {
+			choice = Integer.parseInt(getUserInput(prompt));
+		} while (choice <= 0 || choice > choices.size());
+		return choice;
+	}
+	
+	public int getUserChoice(String title, List<String> choices) {
+		return getUserChoice(title, choices, "Choose a menu item");
+	}
 	
 	/*
 	 * Prints menu and prompts user input
@@ -22,110 +83,141 @@ public class MyAuction {
 		System.out.println();
 		// print choices
 		List<String> choices = null;
+		int choice = 0;
 		switch(menu) {
 			case 2:
-				System.out.println("Administrator Menu\n" + HR);
 				choices = Arrays.asList(
 					"New customer registration",
 					"Update system date",
-					"Statistics"
+					"Statistics",
+					"Logout"
 				);
+				choice = getUserChoice("Administrator menu", choices);
 				break;
 			case 1:
-				System.out.println("User Menu\n" + HR);
 				choices = Arrays.asList(
 					"Browse products",
 					"Search products",
 					"Auction product",
 					"Bid on product",
 					"Sell product",
-					"Show suggestions"
+					"Show suggestions",
+					"Logout"
 				);
+				choice = getUserChoice("User menu", choices);
 				break;
 			default:
-				System.out.println("Main Menu\n" + HR);
 				choices = Arrays.asList(
 					"Administrator login",
-					"User login"
+					"User login",
+					"Exit"
 				);
+				choice = getUserChoice("Main menu", choices);
 				break;
 		}
-		for (int i = 1; i <= choices.size(); i++) {
-			System.out.println("  " + i + ") " + choices.get(i - 1));
-		}
-		System.out.println(HR);
 		
-		// prompt for user input
-		Scanner in = new Scanner(System.in);
-		boolean valid = false;
-		do {
-			System.out.print("Choose a menu item: ");
-			int choice = in.nextInt();
-			
-			// if input is valid, execute task
-			if (choice > 0 && choice <= choices.size()) {
-				System.out.println("\n" + choices.get(choice - 1));
-				if (menu == 2) {
-					// deal with admin menu choices
-				} else if (menu == 1) {
-					// deal with user menu choices
-				} else {
-					// deal with main menu choices
-					switch(choice) {
-						case 1:
-							if(login(1))
-								promptMenu(2);
-							else
-								System.out.println("Error! Invalid username/password!") ;
-							break;
-						case 2:	
-							if(login(2))
-								promptMenu(1);
-							else
-								System.out.println("Error! Invalid username/password!") ;
-							break;
-						default:
-							break;
-					}
-				}
-				valid = true;
+		// handle user's choice
+		System.out.println("\n" + choices.get(choice - 1));
+		if (menu == 2) {
+			// deal with admin menu choices
+			switch(choice) {
+				case 1:
+					// New customer registration
+					break;
+				case 2:
+					// Update system date
+					break;
+				case 3:
+					// Statistics
+					break;
+				default:
+					promptMenu(0);
+					break;
 			}
-		} while(!valid);
+		} else if (menu == 1) {
+			// deal with user menu choices
+			switch(choice) {
+				case 1:
+					// Browse products
+					break;
+				case 2:
+					// Search products
+					break;
+				case 3:
+					// Auction product
+					break;
+				case 4:
+					// Bid on product
+					break;
+				case 5:
+					// Sell product
+					break;
+				case 6:
+					// Show suggestions
+					break;
+				default:
+					promptMenu(0);
+					break;
+			}
+		} else {
+			// deal with main menu choices
+			switch(choice) {
+				case 1:
+					// administrator login
+					if (login(1)) {
+						promptMenu(2);
+					} else {
+						System.out.println("Error! Invalid username/password!");
+						promptMenu(0);
+					}
+					break;
+				case 2:	
+					// user login
+					if (login(2)) {
+						promptMenu(1);
+					} else {
+						System.out.println("Error! Invalid username/password!");
+						promptMenu(0);
+					}
+					break;
+				default:
+					System.out.println("Goodbye!");
+					break;
+			}
+		}
 	}
 	
-	public boolean login(int type) //Basic login function
-	{
-		try{
-	
-		if(type != 1 && type != 2)
-			System.out.println("You should never ever see this output.") ;
-	
-		Scanner scan = new Scanner(System.in) ;
-		String username = null ;
-		String password = null ;
-		System.out.println("\nPlease enter your login information.\nUsername: ") ;
-		username = scan.nextLine() ;
-		System.out.println("Password: ") ;
-		password = scan.nextLine() ;
-		
-		//checking to make sure the usn/pwd match something in the database
-		statement = connection.createStatement() ;
-		if(type == 2) //Which database to check for the login info
-			query = "select login, password from customer" ;
-		else
-			query = "select login, password from administrator" ;
-		resultSet = statement.executeQuery(query) ;
-		while(resultSet.next())
-		{
-			if(username.equals(resultSet.getString(1)) && password.equals(resultSet.getString(2)))
-				return true ; //Username/password combo was found!
+	/*
+	 * @param type 1:admin, 2:user
+	 * @return successful or invalid login
+	 */
+	public boolean login(int type) {
+		try {
+
+			if(type != 1 && type != 2)
+				System.out.println("You should never ever see this output.") ;
+			
+			System.out.println("\nPlease enter your login information.");
+			String username = getUserInput("Username");
+			String password = getUserInput("Password");
+
+			//checking to make sure the usn/pwd match something in the database
+			statement = connection.createStatement() ;
+			if(type == 2) //Which database to check for the login info
+				query = "select login, password from customer" ;
+			else
+				query = "select login, password from administrator" ;
+			resultSet = statement.executeQuery(query) ;
+			while(resultSet.next())
+			{
+				if(username.equals(resultSet.getString(1)) && password.equals(resultSet.getString(2)))
+					return true ; //Username/password combo was found!
+			}
+			return false ; //If there was no match for the username/password, return false
+
 		}
-		return false ; //If there was no match for the username/password, return false
-		
-		}
-		catch(Exception Ex) {
-	    System.out.println("Error running the sample queries.  Machine Error: " +
-			       Ex.toString());
+		catch(Exception e) {
+			System.out.println("Error running database queries: " + e.toString());
 		}
 		return false ; //This should really never happen. I'm just making java happy
 	}
@@ -133,18 +225,17 @@ public class MyAuction {
 	public void browse()
 	{
 		try{
-	
-		Scanner scanner = new Scanner(System.in) ;
-		System.out.println("What category do you want to searh in?") ;
-		String input = scanner.nextLine() ;
+		String input = getUserInput("What category do you want to search in?");
 		
 		statement = connection.createStatement() ;
 		//Need some way of listing the categories to get more specific.
 		//Any idea for the best way to do this?
 		
-		System.out.println("How do you want your products sorted by? (enter the corresponding number)\n"
-		+ "1. Highest Bid First\n2.Lowest Bid First\n3. Alphabetically by product name") ;
-		int sort = scanner.nextInt() ;
+		int sort = getUserChoice("How do you want your products sorted by?", Arrays.asList(
+			"Highest bid first",
+			"Lowest bid first",
+			"Alphabetically by product name"
+		), "Choose a sort option");
 		if(sort == 1)
 			query = "" ; //Will be added after categories
 		else if(sort == 2)
@@ -168,12 +259,10 @@ public class MyAuction {
 	{
 		try{
 	
-		Scanner scanner = new Scanner(System.in) ;
-		System.out.println("Please enter the first keyword you would like to search by: ") ;
-		String input = scanner.nextLine() ;
-		System.out.println("Enter the second keyword you would like to search by, or leave "
-		+ "this field blank if you only want to use one keyword: " ) ;
-		String input2 = scanner.nextLine() ;
+		
+		String input = getUserInput("Please enter the first keyword you would like to search by");
+		String input2 = getUserInput("Enter the second keyword you would like to search by, or leave "
+			+ "this field blank if you only want to use one keyword");
 		
 		PreparedStatement updateStatement ;
 		if(input2.equals(""))
@@ -201,31 +290,6 @@ public class MyAuction {
 		catch(Exception Ex) {
 	    System.out.println("Error running the sample queries.  Machine Error: " +
 			       Ex.toString());
-		}
-	}
-	
-	public MyAuction() {
-		try {
-			// get username and password
-			Scanner scanner = new Scanner(new File("db.config"));
-			username = scanner.nextLine();
-			password = scanner.nextLine();
-			scanner.close();
-			
-			try {
-				// register oracle driver and connect to db
-				// must be on unixs.cis.pitt.edu and run "source ~panos/1555/bash.env"
-				DriverManager.registerDriver (new oracle.jdbc.driver.OracleDriver());
-				connection = DriverManager.getConnection("jdbc:oracle:thin:@db10.cs.pitt.edu:1521:dbclass", username, password);
-				
-				System.out.println("Welcome to MyAuction!");
-				promptMenu(0);
-			}
-			catch(Exception e)  {
-				System.err.println("Error connecting to database: " + e.toString());
-			}
-		} catch (FileNotFoundException e) {
-			System.err.println("Could not find db.config file: " + e.toString());
 		}
 	}
 
